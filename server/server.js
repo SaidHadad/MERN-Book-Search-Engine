55521e474c7132dcdc9a74287778d94b26912bf6
SaidHadad <saiddavid.hadad@gmail.com>
2021-09-12 12:05:51 -0400

    Resolvers and typeDefs
    Added resolvers and typedefs,
    for now the server folder is secluded from the client.
    Connection with playgrounds Ok.
    ToDo.- Connect server and client side first creating the queries.js and mutations.js files

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 // import apollo server from apollo-server-expxress
 const { ApolloServer } = require('apollo-server-express')
-const path = require('path');
+// const path = require('path'); -----------------------
 
 // const routes = require('./routes');
 // changed configure through routes for the typeDefs and resolver schemas
 const {typeDefs, resolvers} = require('./schemas');
 // middleware for the authentication
-const {authMiddleware} = require('./utils/auth');
+// const {authMiddleware} = require('./utils/auth');
 
 const db = require('./config/connection');
 
@@ -17,7 +17,7 @@ const app = express();
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers, 
-  context: authMiddleware 
+  // context: authMiddleware 
 });
 
 server.applyMiddleware({ app });
@@ -26,13 +26,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
